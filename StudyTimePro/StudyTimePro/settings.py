bb95d713b13b34f34bf2db025699deeba8824c8b
spinedace <spinedace@unal.edu.co>
2023-11-13 22:29:18 -0500

    configuracion HOST deploy

--- StudyTimePro/StudyTimePro/settings.py
+++ StudyTimePro/StudyTimePro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mq=_0c+hk1)h14qm)8)uzpbu&2x&lpd&%vtm^0&!w(tbkaftht
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studytimepro.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
